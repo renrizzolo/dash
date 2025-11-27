e9d185a463dbc08b596fac70bca074d22ab9360a
Ren Rizzolo <renrizmusic@gmail.com>
2025-11-27 11:48:57 +1100

    fix relative, add manual timezone offset

--- packages/frontend/src/components/TrainDepartures.tsx
+++ packages/frontend/src/components/TrainDepartures.tsx
@@ -8,11 +8,6 @@ interface TrainDeparturesProps {
 }
 
 const TrainDepartures: Component<TrainDeparturesProps> = (props) => {
-	const formatTime = (dateString: string) => {
-		const date = new Date(dateString);
-		return date.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true });
-	};
-
 	const relativeTime = (departureDate: string, showSeconds: boolean) => {
 		const date = new Date(departureDate);
 		const now = props.dateTimeNow;
@@ -50,12 +45,13 @@ const TrainDepartures: Component<TrainDeparturesProps> = (props) => {
 				<Match when={props.departures && props.departures.length > 0}>
 					<For each={props.departures} fallback={null}>
 						{(departure, index) => {
-							const relative = relativeTime(departure.estimated_departure_utc || departure.scheduled_departure_utc, index() === 0);
+							const relative = () => relativeTime(departure.estimated_departure_utc || departure.scheduled_departure_utc, index() === 0);
 							const isDelayedOrEarly =
 								departure.estimated_departure_utc && departure.scheduled_departure_utc !== departure.estimated_departure_utc;
+
 							return (
 								<div
-									class={'departure-item' + (index() === 0 ? ' departure-item-next' : '') + (relative === 'departed' ? ' departed' : '')}
+									class={'departure-item' + (index() === 0 ? ' departure-item-next' : '') + (relative() === 'departed' ? ' departed' : '')}
 								>
 									<div class="departure-info">
 										<span>
@@ -65,15 +61,15 @@ const TrainDepartures: Component<TrainDeparturesProps> = (props) => {
 									<div class="departure-info">
 										<span class="departure-time">
 											<time class={isDelayedOrEarly ? 'delayed' : ''}>
-												{formatTime(departure.scheduled_departure_utc)} {isDelayedOrEarly && <br />}
+												{formatUTCDateToLocal(departure.scheduled_departure_utc)} {isDelayedOrEarly && <br />}
 											</time>
-											{isDelayedOrEarly && <time>{formatTime(departure.estimated_departure_utc!)} </time>}
+											{isDelayedOrEarly && <time>{formatUTCDateToLocal(departure.estimated_departure_utc!)} </time>}
 											<span class="relative-time">
-												<Switch fallback={relative}>
-													<Match when={relative === 'departed'}>
+												<Switch fallback={relative()}>
+													<Match when={relative() === 'departed'}>
 														<span class="departed">Departed</span>
 													</Match>
-													<Match when={relative === 'departing'}>
+													<Match when={relative() === 'departing'}>
 														<span class="departing">Departing</span>
 													</Match>
 												</Switch>
@@ -92,3 +88,42 @@ const TrainDepartures: Component<TrainDeparturesProps> = (props) => {
 };
 
 export default TrainDepartures;
+
+/**
+ * Format a UTC date string to Australian Eastern Time
+ *
+ * Note: not using date.toLocaleString because the kindle browser
+ * I'm using this on is set to GMT+00 and doesn't support timezones properly.
+ */
+function formatUTCDateToLocal(dateString: string): string {
+	const date = new Date(dateString);
+
+	const utcHours = date.getUTCHours();
+	const utcMinutes = date.getUTCMinutes();
+
+	const offset = getManualOffset(dateString);
+	const localHours = utcHours + offset;
+
+	const localDate = new Date();
+	localDate.setHours(localHours, utcMinutes, 0, 0);
+
+	return localDate.toLocaleTimeString('en-US', {
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: true,
+	});
+}
+
+/** Get the timezone offset for a UTC date to Australian Eastern Time */
+function getManualOffset(dateString: string): number {
+	const date = new Date(dateString);
+	const month = date.getMonth();
+
+	if (month >= 9 || month <= 2) {
+		// AEDT
+		return 11;
+	} else {
+		// AEST
+		return 10;
+	}
+}
