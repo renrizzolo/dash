448f9cc9ea2d4ae6c7e9fb1069c377c6dfdea775
Ren Rizzolo <renrizmusic@gmail.com>
2026-03-18 19:56:58 +1100

    UI various fixes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App(props: RouteSectionProps) {
 	return (
 		<ToastProvider>
 			<div class="w-full stack-stretch-4 text-center font-sans max-w-container bg-default stack-center-4 my-0 mx-auto sm:p-8 p-4">
-				<nav>
+				<nav class="w-max self-center">
 					<Tabs>
 						<TabLink href="/recipes">Recipes</TabLink>
 						<TabLink href="/train">Train</TabLink>
